0756bf1d3e9f5ce96ce99c48a5462c2460388f29
cejnik <chejnma@gmail.com>
2024-12-03 22:07:26 +0100

    JS1

---
+++ Javascript/JavaScript 1/3const.js
@@ -0,0 +1,7 @@
+const firstName = "martin";
+const color = "blue";
+console.log(firstName, color);
+
+let city = "české budějovice";
+/* let*/ city = "praha"; //nevtvářím další proměnou
+console.log(city);
---
+++ Javascript/JavaScript 1/script.js
@@ -0,0 +1,3 @@
+const color = prompt("Vyber barvu: ");
+const body1 = document.querySelector("body");
+body1.style.backgroundColor = color;
