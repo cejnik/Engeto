356abdea8a623fa3ce57c072c787559a14387eab
cejnik <chejnma@gmail.com>
2024-12-11 19:32:57 +0100

    extend

--- Javascript/JavaScript8/script.js
+++ Javascript/JavaScript8/script.js
@@ -1,56 +1,62 @@
 class CoffeeMachine {
   constructor() {
-    this.waterlevel = 150; //výchozí množství vody v ml
+    this.waterLevel = 150;
   }
+
   /**
+   * Indikuje, že je potřeba doplnit vodu
    *
    * @returns boolean
    */
-  needsRefil() {
-    return this.waterlevel < 200;
+  needsRefill() {
+    return this.waterLevel < 200;
   }
 
   /**
-   * Tato funnkce přidíví vody v zadaném množství do zásobníku
-   * @param {*} amount
+   * Přidává vodu v zadaném množství do zásobníku
    *
-   * @param number Možnoství vody v mol.
+   * @throws Argument není číslo
+   *
+   * @param Number Množství vody v ml
    */
-
   addWater(amount) {
-    if (typeof amount !== "number")
+    if (typeof amount !== "number") {
       throw new Error("Parameter is not a number");
-    this.waterlevel += amount;
+    }
+
+    this.waterLevel = this.waterLevel + amount;
     console.log(
-      `Added ${amount}ml of water. Currenly there is ${this.waterlevel} ml of water.`
+      `Added ${amount}ml of water, currently theres ${this.waterLevel}ml of water`
     );
   }
 
   /**
-   * tato funkce vaří kávu, nejdřív zkontroluje hladinu vody v zásobníku.
+   * Tato funkce vaří kávu, nejdříve zkontroluje hladinu vody v zásobníku
    *
-   * @throws Není dostatek vody
+   * @throws Není dostatek vody v zásobníku
    */
-
   brew() {
-    if (this.needsRefil()) {
-      throw new Error("Je nutné přidat vodu.");
+    if (this.needsRefill()) {
+      throw new Error("Need to refeill the water");
     }
 
-    this.waterlevel = this.waterlevel - 200;
-    console.log("Here is your coffee");
+    this.waterLevel = this.waterLevel - 200;
+    console.log("Brewing coffee");
   }
 }
 
-const newCoffeeMachine = new CoffeeMachine();
+/**
+ * Obsahuje objekt kávovaru ze třídy CoffeeMachine
+ */
+/* const myCoffeeMachine = new CoffeeMachine;
 
-try {
-  newCoffeeMachine.brew();
-} catch (e) {
-  newCoffeeMachine.addWater(50);
+try{
+  myCoffeeMachine.brew();
+} catch(error){
+  myCoffeeMachine.addWater(200);
 }
 
-newCoffeeMachine.brew();
+myCoffeeMachine.brew(); */
 
 class TeaMachine extends CoffeeMachine {
   /**
@@ -62,3 +68,16 @@ class TeaMachine extends CoffeeMachine {
     return this.waterLevel < 500;
   }
 }
+
+/**
+ * Obsahuje objekt kávovaru ze třídy CoffeeMachine
+ */
+const myTeaMachine = new TeaMachine();
+
+try {
+  myCoffeeMachine.brew();
+} catch (error) {
+  myCoffeeMachine.addWater(200);
+}
+
+myCoffeeMachine.brew();
