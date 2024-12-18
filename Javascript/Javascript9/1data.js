e62a50f5d2643b201efdfb8c32467012a8f199c6
cejnik <chejnma@gmail.com>
2024-12-18 21:04:57 +0100

    API

---
+++ Javascript/Javascript9/1data.js
@@ -0,0 +1,53 @@
+const quotes = [
+  [
+    {
+      title: "Bez práce nejsou koláče",
+      country: "CZ",
+    },
+    1,
+  ],
+  [
+    {
+      title: "Darovanému koni na zuby nekoukej",
+      country: "CZ",
+    },
+    2,
+  ],
+  [
+    {
+      title: "Co je šeptem, to je čertem",
+      country: "CZ",
+    },
+    3,
+  ],
+  [
+    {
+      title: "Kto si myslí, že vie všetko, nič nevie.",
+      country: "SK",
+    },
+    4,
+  ],
+  [
+    {
+      title: "Ranní ptáče dále doskáče",
+      country: "CZ",
+    },
+    5,
+  ],
+  [
+    {
+      title: "Kam nechodí slunce, musí lékař",
+      country: "CZ",
+    },
+    6,
+  ],
+  [
+    {
+      title: "Chybami se člověk učí",
+      country: "CZ",
+    },
+    7,
+  ],
+];
+
+console.log(quotes[0][0].country);
