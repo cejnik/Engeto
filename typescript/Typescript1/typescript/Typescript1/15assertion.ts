ec83c24440ac75dd92f45353c690a8e55083fc22
cejnik <chejnma@gmail.com>
2025-01-12 21:25:26 +0100

    Delete typescript/Typescript1/typescript/Typescript1 directory

--- typescript/Typescript1/typescript/Typescript1/15assertion.ts
+++
@@ -1,5 +0,0 @@
-const value: unknown = "Data z neznámého zdroje";
-
-const strLength = (value as string).length; //přetypuje hodnotu value na string a poté spočítá její délku
-
-console.log(strLength);
--- typescript/Typescript1/typescript/Typescript1/4funkceMap.ts
+++
@@ -1,7 +0,0 @@
-const students = ["david", "hermiona", "Ron"];
-
-const newStudents = students.map((oneStudent: string) => {
-  return oneStudent.toUpperCase();
-});
-
-console.log(newStudents);
