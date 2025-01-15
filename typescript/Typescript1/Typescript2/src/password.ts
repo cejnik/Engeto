a82b7cdf2420bf639472fc7843a5c165429d32f3
cejnik <chejnma@gmail.com>
2025-01-15 21:02:17 +0100

    Add files via upload

---
+++ typescript/Typescript1/Typescript2/src/password.ts
@@ -0,0 +1,1 @@
+export const password: string = "sdwqd5qwd6qw";
---
+++ typescript/Typescript1/Typescript2/src/script.ts
@@ -0,0 +1,6 @@
+import { password } from "./password.js"; //vždy z javascriptu
+
+const first: number = 1;
+const second: number = 2;
+
+console.log(password);
